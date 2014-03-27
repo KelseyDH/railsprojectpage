58a1af79432dee08dbdaf93e04e9719f13634c67
KelseyDH <khannan@uvic.ca>
2014-03-26 22:14:16 -0700

    Project Rails Page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+
+  def index
+  end
+
+  def about
+  end
+
+  def contactme
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Projectman::Application.config.secret_key_base = '374c504c94b96ec7339a9ffce6c15e1f141313c82c728d466985ae78044e40669226dfbb3b31767a9a61a630324d67121897a3a84971cdfd9cb23ea7402f252d'
